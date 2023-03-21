a87ce04f5c92d89378cc2abdbb89baac0cc0bf61
Simone Daglio <simone.daglio823@outlook.it>
2023-03-21 10:57:58 +0100

    classe book

---
+++ lib/book.dart
@@ -0,0 +1,30 @@
+// Classe book
+class Book {
+  // variabili
+  late String id;
+  late String title;
+  late String authors;
+  late String description;
+  late String editor;
+  late String image;
+
+  // costruttore unnamed
+  Book(
+    this.id,
+    this.title,
+    this.authors,
+    this.description,
+    this.editor,
+    this.image
+  );
+
+  // costruttore con nome from map
+  Book.fromMap(Map<String, dynamic> el) {
+    id = el['id'];
+    title = el['volumeInfo']['title'].toString();
+    authors = el['volumeInfo']['authors'] == null ? '' : el['volumeInfo']['authors'].toString();
+    description = el['volumeInfo']['description'] == null ? '' : el['volumeInfo']['description'].toString();
+    editor = el['volumeInfo']['publisher'] == null ? '' : el['volumeInfo']['publisher'].toString();
+    image = el['volumeInfo']['imageLinks']['smallThumbnail'] == null ? '' : el['volumeInfo']['imageLinks']['smallThumbnail'].toString();
+  }
+}
