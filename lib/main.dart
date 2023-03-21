3ea4066bb3e7789ec695da8405c06ab7ff1f379e
Simone Daglio <simone.daglio823@outlook.it>
2023-03-21 10:01:36 +0100

    http get google api book

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 // IMPORTS
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(const RootApp());
@@ -19,7 +20,75 @@ class RootApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const Text('Home')
+      home: const LibriScreen()
+    );
+  }
+}
+
+
+// Classi libri screen
+class LibriScreen extends StatefulWidget {
+  // costruttore
+  const LibriScreen({super.key});
+
+  @override
+  State <LibriScreen> createState() => _LibriScreenState();
+}
+
+class _LibriScreenState extends State<LibriScreen> {
+  // init state
+  @override
+  void initState() {
+    searchBooks('Oceano Mare');
+    super.initState();
+  }
+
+  // risultato
+  String result = "";
+
+  // cerca libri
+  Future searchBooks(String ricerca) async {
+    // scomposizione url
+    const dominio = 'www.googleapis.com';
+    const percorso = '/books/v1/volumes';
+
+    // query ricerca
+    Map <String, dynamic> params = {
+      'q': ricerca
+    };
+
+    // url finale
+    final Uri url = Uri.https(dominio, percorso, params);
+
+    // get data
+    http.get(url).then((res) {
+      setState(() {
+        result = res.body;
+      });
+    });
+
+    setState(() {
+      result = 'Richiesta in corso';
+    });
+  }
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Libri',
+          style: TextStyle(
+            fontWeight: FontWeight.bold
+          )
+        ),
+        backgroundColor: Colors.lightBlue,
+      ),
+      body: Container(
+        child: Text(result),
+      ),
     );
   }
 }
